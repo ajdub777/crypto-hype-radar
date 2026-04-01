4654881e56a911cb9b378b65d9005df54601c259
ajdub777 <adamjwconsulting@gmail.com>
2026-03-31 22:14:00 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
+self.addEventListener('push', event => {
+  const data = event.data ? event.data.json() : { title: 'Crypto Hype Radar', body: 'New signal detected!' };
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/favicon.ico',
+      badge: '/favicon.ico',
+      vibrate: [200, 100, 200],
+      data: { url: 'https://cryptohyperadar.com' },
+      actions: [{ action: 'view', title: 'View Signal' }]
+    })
+  );
+});
 
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('https://cryptohyperadar.com'));
+});
